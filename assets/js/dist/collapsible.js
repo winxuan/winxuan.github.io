3b12c280d308a9cc7858ff1f5b513ae3e0f7eb28
Marvin <447012540@qq.com>
2024-01-07 06:16:28 +0800

    增加默认展开功能，删除原hardcode

--- assets/js/dist/collapsible.js
+++ assets/js/dist/collapsible.js
@@ -1,11 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
     let coll = document.getElementsByClassName("collapsible-container");
-    let maxLines = 5; // 设置折叠显示的行数 Maximum number of lines to display without collapsing
+    let maxLines = 10; // 设置折叠显示的行数 Maximum number of lines to display without collapsing
+    let defaultOpenLines = 30; // 默认展开的最大行数
 
     for (let i = 0; i < coll.length; i++) {
       let trigger = coll[i].querySelector('.collapsible-trigger');
       let content = coll[i].querySelector('.collapsible-content');
-      let codeLines = content.textContent.split('\n').length;
+      let codeLines = (content.textContent.split('\n').length - 1) / 2; // 计算实际的代码行数
    
       /* codeLines=实际行的行数*2+一行换行空行，因为chirpy主题使用了<table>标签，里边包含两列，所以js会将1行代码视为2行。下边被注释掉的代码可以查看codeLines的真实值。codeLines=real_lines*2+1wrap_blank_line,because the chirpy theme uses <table> with 2 columns,which js reguard 1 line as 2 lines. The code section below can help you see the real value of codeLines.
       let tempcodeLines = content.textContent.split('\n').length;
@@ -13,9 +14,13 @@ document.addEventListener("DOMContentLoaded", function () {
       lineCount.textContent = ' (' + tempcodeLines + ' lines)';
       coll[i].appendChild(lineCount);
       */
-   
-      if (codeLines - 6 <= maxLines) { /*根据主题的实际情况将codeLines调整为实际行数 Adjust codeLines to real lines*/
-        trigger.style.display = 'none';
+      console.log(codeLines)
+
+      if (codeLines <= defaultOpenLines) {
+        // 如果代码行数小于或等于20行，则默认展开
+        this.innerHTML = "收起";
+        trigger.style.display = 'none'; // 隐藏触发器
+        content.style.maxHeight = content.scrollHeight + "px";
       } else {
         trigger.addEventListener("click", function () { 
           // 切换按钮上的文字
